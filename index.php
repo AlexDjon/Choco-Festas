2eab9ce3c172632346b72ca0e5cbe9fbed039f38
AlexDjon <alexdjonata30@gmail.com>
2020-02-01 19:13:50 -0300

    Altered

--- index.php
+++ index.php
@@ -10,30 +10,36 @@ for($i = 0; $i < count($procurarViews); $i++) {
 	$procurarViews[$i] = explode('.php', explode('/', $procurarViews[$i])[1])[0];
 }
 
-//. Procura na pasta views
+
+function loged() {
+	if(isset($_COOKIE['logado']) && $_COOKIE['logado'] == 1) {
+		return true;
+	} 
+	else {
+		return false;
+	}
+}
+
+//. Procura na pasta views e se não achar vai ser incluso a pagina inicial ou de login
 if (isset($_GET['p'])) {
 	$pagina = trim(urldecode($_GET['p']));
 
+	// Verificar path
 	for ($i = 0; $i < count($procurarViews); $i++) {
 		if($pagina == $procurarViews[$i]) {
-            $retorno = $pagina;
+            $req = loged() ? $pagina : 'login';
 		}
-	}
-	if (isset($retorno)) {
-		// Verificar login
-		if(!isset($_COOKIE['logado']) || $_COOKIE['logado'] != 1) {
-			require_once SITE_VIEWS.'login.php';
-		}
-		else {
-			require_once SITE_VIEWS.$retorno.'.php';
-		}
-	}
-	else {
-		require_once SITE_VIEWS.'undefined.html';
 	}
 }
 else {
-	header('Location: '.SITE_RAIZ.'inicio');
+	if(loged()) {
+		$req = 'inicio';
+	}
+	else {
+		$req = 'login';
+	}
 }
+
+require_once SITE_VIEWS.$req.'.php';
 
 ?>
