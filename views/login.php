3c6a5424c4a12064f6f582f1dcb0d9797e8c0cc0
AlexDjon <alexdjonata30@gmail.com>
2020-02-18 18:25:25 -0300

    login

--- views/login.php
+++ views/login.php
@@ -3,7 +3,17 @@
 <head>
     <meta charset='utf-8'>
     <title><?= SITE_NAME ?> - Login</title>
+
+    <!-- Meta tags -->
+    <meta name="author" content="Alex Djonata - htps://github.com/AlexDjonata">
+    <meta name="description" content="Sistema de Anotações de Pedidos, utilizando JavaScript e ajax para fazer requisições no Php - Feito para um amigo.">
+    <meta name="keywords" content="alex djonata, chocofestas, choco, festas">
+    <meta name="copyright" content="© 2020 Alex Djonata">
+    <meta name="theme-color" content="#4DD3AF">
+    <meta name="robots" content="index, follow">
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <!-- Links -->
     <link rel='shortcut icon' href='<?= SITE_IMAGES ?>favicon.png'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
